0630d0d38865db1206e47cc95c4976ca2d40c2b4
luvizzz <pedro.luvizotto@gmail.com>
2020-03-10 01:03:35 +0100

    Begin modelling domain entities.
    Append target files to git ignore.

---
+++ taf/src/test/java/domain/Car.java
@@ -0,0 +1,33 @@
+package domain;
+
+public class Car {
+    private long id;
+
+    private String name;
+
+    private Manufacturer manufacturer;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Manufacturer getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(Manufacturer manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+}
