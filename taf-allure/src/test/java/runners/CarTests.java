6106858fbc7f1f9ba591a318c35286fc03f19b7f
Pedro Luvizotto <pedro_luvizotto@epam.com>
2021-03-28 23:42:46 +0200

    Move taf to taf-allure

--- taf-allure/src/test/java/runners/CarTests.java
+++ taf-allure/src/test/java/runners/CarTests.java
@@ -4,9 +4,13 @@ import base.BaseTest;
 import domain.Car;
 import domain.Country;
 import domain.Manufacturer;
-import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assumptions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import java.util.List;
 import java.util.UUID;
