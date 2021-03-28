af0615df6ecc7e38d4e55d4cc53e735da61c28af
Pedro Luvizotto <pedro_luvizotto@epam.com>
2021-03-29 00:16:48 +0200

    Make taf-allure use allure-restAssured filter

--- taf-allure/src/test/java/base/BaseSteps.java
+++ taf-allure/src/test/java/base/BaseSteps.java
@@ -1,6 +1,8 @@
 package base;
 
+import io.qameta.allure.restassured.AllureRestAssured;
 import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
 
@@ -8,10 +10,13 @@ public class BaseSteps {
     protected final static String ROOT_URI = "http://localhost:8080";
 
     protected RequestSpecification given() {
+        RequestSpecification spec = new RequestSpecBuilder()
+                .addFilter(new AllureRestAssured())
+                .build();
+
         return RestAssured.given().baseUri(ROOT_URI)
                 .contentType(ContentType.JSON)
-                //.contentType("application/json")
-                //.header("Accept","*/*")
+                .spec(spec)
         ;
     }
 }
